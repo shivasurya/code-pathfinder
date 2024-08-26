efa5a17030aae2a451b34044c4a6c14a988e10b4
Shivasurya <s.shivasurya@gmail.com>
2024-08-25 23:19:08 -0400

    support multiple entity selection via cartesian product 🏫👩‍🏫📚🔢📐

--- test-src/android/app/src/test/java/com/ivb/udacity/ExampleUnitTest.java
+++ test-src/android/app/src/test/java/com/ivb/udacity/ExampleUnitTest.java
@@ -9,7 +9,7 @@ import static org.junit.Assert.*;
  */
 public class ExampleUnitTest {
     @Test
-    public void addition_isCorrect() throws Exception {
+    public void is_addition_correct() throws Exception {
         assertEquals(4, 2 + 2);
     }
 }
