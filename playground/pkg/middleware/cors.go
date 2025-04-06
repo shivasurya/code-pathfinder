91be9399783998b338c761840678c796d1d617f8
shivasurya <s.shivasurya@gmail.com>
2025-04-05 23:11:45 -0400

    allowlist cpf main domain

--- playground/pkg/middleware/cors.go
+++ playground/pkg/middleware/cors.go
@@ -5,8 +5,7 @@ import "net/http"
 // CorsMiddleware adds CORS headers to allow requests from localhost:4321 during development
 func CorsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// Allow requests from localhost:4321 during development
-		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:4321")
+		w.Header().Set("Access-Control-Allow-Origin", "https://codepathfinder.dev")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 
